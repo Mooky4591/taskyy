86be32cc979481959d534a1c0a6820df269a8113
Scott Robinson <SRobinson2@harriscomputer.com>
2024-02-29 11:52:09 -0500

    Finished creating DB and set up insertion of the newly registerd userEntity. Deleted unneeded user object since I am using UserEntity.

--- app/src/main/java/com/example/taskyy/data/data_access_objects/UserDao.kt
+++ app/src/main/java/com/example/taskyy/data/data_access_objects/UserDao.kt
@@ -4,14 +4,14 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import com.example.taskyy.domain.User
+import com.example.taskyy.data.room_entity.UserEntity
 
 @Dao
 interface UserDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertUser(user: User)
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertUser(userEntity: UserEntity)
 
     @Delete
-    suspend fun deleteUser(user: User)
+    suspend fun deleteUser(userEntity: UserEntity)
 
 }
--- app/src/main/java/com/example/taskyy/di/AuthModule.kt
+++ app/src/main/java/com/example/taskyy/di/AuthModule.kt
@@ -1,7 +1,6 @@
 package com.example.taskyy.di
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import android.content.Context
 import com.example.taskyy.data.data_access_objects.UserDao
 import com.example.taskyy.data.repository.AuthRepositoryImpl
 import com.example.taskyy.data.room_database.TaskyyDatabase
@@ -10,6 +9,7 @@ import com.example.taskyy.domain.repository.AuthRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 @Module
@@ -29,11 +29,9 @@ object AuthModule {
         return LoginUseCase()
     }
 
-    @Composable
     @Provides
     @Singleton
-    fun provideUserDao(): UserDao {
-        //this doesn't work since it needs the composable annotation. How to get the context here?
-        return TaskyyDatabase.getDatabase(LocalContext.current).userDao()
+    fun provideUserDao(@ApplicationContext context: Context): UserDao {
+        return TaskyyDatabase.getDatabase(context = context).userDao()
     }
 }
