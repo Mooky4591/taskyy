eb47d3e19add950ed962eb4f31cec2cb3f71cbb2
Scott Robinson <SRobinson2@harriscomputer.com>
2024-03-01 16:27:11 -0500

    Added additional methods to authmodule

--- app/src/main/java/com/example/taskyy/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/taskyy/data/remote/RetrofitInstance.kt
@@ -4,12 +4,4 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-
-val api: TaskyyApi by lazy {
-    Retrofit.Builder()
-        .baseUrl("https://tasky.pl-coding.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(TaskyyApi::class.java)
-    }
 }
